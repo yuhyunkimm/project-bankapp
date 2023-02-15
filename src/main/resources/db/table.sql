a9848cc732583bb79d60ea9a1d33ebf7eaaba3f6
YouHyunKim <yuhyunk7@gmail.com>
2023-02-15 11:05:05 +0900

    table 더비 생성

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -2,8 +2,23 @@ CREATE TABLE user_tb(
     id int auto_increment primary key,
     username varchar unique not null,
     password varchar not null,
-    email varchar not null,
-    profile varchar, 
+    fullname varchar not null,
+    created_at timestamp not null
+);
+CREATE TABLE account_tb(
+    id int auto_increment primary key, 
+    number varchar unique not null,
+    password varchar not null,
+    balance bigint not null,
+    user_id int,
+    created_at timestamp not null
+);
+CREATE TABLE history_tb(
+    id int auto_increment primary key,
+    amount bigint not null,
+    w_balance bigint,
+    w_account int,
+    d_account int,
     created_at timestamp not null
 );
 commit;
